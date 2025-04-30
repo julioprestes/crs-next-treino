9613a9ff2f171c5df3220ddd5b6074be03f5bedd
julio <juliocmp@unochapeco.edu.br>
2025-04-30 18:17:49 -0300

    primeiro commit

---
+++ src/app/tasks/page.js
@@ -0,0 +1,213 @@
+'use client'
+import {
+    Box,
+    Heading,
+    Flex,
+    Input,
+    Button,
+    Stack,
+    Table,
+    Checkbox,
+    Popover,
+    Portal,
+    Text,
+    Group
+} from "@chakra-ui/react"
+import { useState } from "react";
+import { MdAdd, MdDelete, MdMode, MdChevronLeft, MdChevronRight } from "react-icons/md";
+import { ButtonGroup, IconButton, Pagination } from "@chakra-ui/react"
+
+export default function Tasks() {
+    const [tasks, setTasks] = useState([]);
+    const [input, setInput] = useState('');
+    const [editValue, setEditValue] = useState('');
+    const [itemsPerPage, setItemsPerPage] = useState(5);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [editIndex, setEditIndex] = useState(null);
+    const [ searchInput, setSearchInput] = useState();
+
+
+    const tarefasFiltradas = searchInput ? tasks.filter(task => task.includes(searchInput.toLowerCase())) : tasks;
+    const indexUltimoItem = currentPage * itemsPerPage;
+    const indexPrimeiroItem = indexUltimoItem - itemsPerPage;
+    const tasksAtuais = tarefasFiltradas.slice(indexPrimeiroItem, indexUltimoItem);
+    
+    
+    const criarTask = () => {
+        if (!input.trim()) return;
+        setTasks([...tasks, input]);
+        setInput('');
+        console.log(tasks);
+    }
+
+    const excluirTask = (index) => {
+        const taskExcluido = tasks.filter((_, i) => i != index);
+        setTasks(taskExcluido);
+    }
+
+    const editarTask = () => {
+        if (!editValue.trim()) return;
+        const novaLista = [...tasks];
+        novaLista[editIndex] = editValue; 
+        setTasks(novaLista);
+        setEditValue(''); 
+        setEditIndex(null); 
+    }
+
+    const handleEditButtonClick = (i) => {
+        const realIndex = indexPrimeiroItem + i;
+        setEditIndex(realIndex);
+        setEditValue(tasks[realIndex]);
+    }
+
+    const trocaPage = (i) => {
+        if (tasksAtuais.length === 0 && currentPage != 1) {
+            setCurrentPage (currentPage - 1)
+        }
+    }
+
+    return (
+        <Box p={8}>
+            <Heading mb={4}> Lista de Tarefas </Heading>
+            <Input
+                    placeholder="Pesquise Tarefas..."
+                    variant="flushed"
+                    mr={2}
+                    mb={4}
+                    value={searchInput}
+                    onChange={(valor) => setSearchInput(valor.target.value)}
+                />
+            <Flex mb={4}>
+                <Input
+                    placeholder="Digite o nome de uma tarefa"
+                    variant= "subtle"
+                    mr={2}
+                    value={input}
+                    onChange={(valor) => setInput(valor.target.value)}
+                />
+                <Button
+                    onClick={criarTask} 
+                    background="green"
+                    color="white"
+                > <MdAdd /> </Button>
+            </Flex>
+            <Stack style={{ display: 'flex', alignItems: 'center' }}>
+                <Table.Root width="50%" size="sm" striped>
+                <Table.Header>
+                    <Table.Row>
+                    <Table.ColumnHeader>Tarefa</Table.ColumnHeader>
+                    <Table.ColumnHeader textAlign="center"></Table.ColumnHeader>
+                    </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                    {tasksAtuais.map((task, i) => (
+                    <Table.Row key={i}>
+                        <Table.Cell>{task}</Table.Cell>
+                        <Table.Cell textAlign="center">
+                            <Stack direction="row">
+                            <Popover.Root>
+                                <Popover.Trigger asChild>
+                                <Button
+                                    background="blue"
+                                    color="white"
+                                    variant="suble"
+                                    size="xs"
+                                    onClick={() => handleEditButtonClick(i)}
+                                >
+                                    <MdMode />
+                                    </Button>
+                                </Popover.Trigger>
+                                <Portal>
+                                    <Popover.Positioner>
+                                    <Popover.Content>
+                                        <Popover.Arrow />
+                                        <Popover.Body>
+                                        <Popover.Title fontWeight="medium">Editar Tarefa</Popover.Title>
+                                        </Popover.Body>
+                                        <Popover.Footer>
+                                            <Group>
+                                                <Input
+                                                    placeholder="Nome da tarefa"
+                                                    size="sm"
+                                                    mt={2}
+                                                    value={editValue}
+                                                    onChange={(evento) => setEditValue(evento.target.value)}
+                                                    
+                                                />
+                                                <Button
+                                                    size="sm"
+                                                    onClick={editarTask}
+                                                >
+                                                    Confirmar
+                                                </Button>
+                                            </Group>
+                                            </Popover.Footer>
+                                    </Popover.Content>
+                                    </Popover.Positioner>
+                                </Portal>
+                            </Popover.Root>
+                            <Button
+                                background="red"
+                                color="white"
+                                variant="suble"
+                                size="xs"
+                                onClick={() => {
+                                    excluirTask(indexPrimeiroItem + i);
+                                    trocaPage(i);
+                                }}
+                            >
+                                <MdDelete />
+                            </Button>
+                            </Stack>
+                        </Table.Cell>
+                    </Table.Row>
+                    ))}
+                </Table.Body>
+                </Table.Root>
+                <Pagination.Root
+                    count={tarefasFiltradas.length}
+                    pageSize={itemsPerPage} 
+                    defaultPage={1}
+                    page={currentPage}
+                    onPageChange={(page) => setCurrentPage(page)}
+                >
+                    <ButtonGroup variant="ghost" size="sm">
+                        <Pagination.PrevTrigger asChild>
+                        <IconButton
+                            onClick={() => setCurrentPage(currentPage - 1)}
+                        >
+                            <MdChevronLeft />
+                        </IconButton>
+                        </Pagination.PrevTrigger>
+
+                        <Pagination.Items
+                        render={(page) => (
+                            <IconButton onClick={() => setCurrentPage(page.value)}
+                            variant={{ base: "ghost", _selected: "outline" }}
+                            >
+                            {page.value}
+                            </IconButton>
+                        )}
+                        />
+
+                        <Pagination.NextTrigger asChild>
+                        <IconButton
+                            onClick={() => setCurrentPage(currentPage + 1)}
+                        >
+                            <MdChevronRight />
+                        </IconButton>
+                        </Pagination.NextTrigger>
+                    </ButtonGroup>
+                </Pagination.Root>
+            </Stack>
+        </Box>
+    )
+}
+
+
+
+
+{/* <Checkbox.Root variant="subtle">
+                            <Checkbox.HiddenInput />
+                            <Checkbox.Control />
+                        </Checkbox.Root> */}
---
+++ src/app/teste/page.js
@@ -0,0 +1,52 @@
+import { Table, Avatar, Button, Card, QrCode} from "@chakra-ui/react"
+
+const Demo = () => {
+  return (
+    <Card.Root width="320px">
+      <Card.Body gap="2">
+      <QrCode.Root value="https://www.google.com">
+        <QrCode.Frame>
+          <QrCode.Pattern />
+        </QrCode.Frame>
+      </QrCode.Root>
+        <Card.Title mt="2">Nue Camp</Card.Title>
+        <Card.Description>
+        <Table.Root size="sm">
+      <Table.Header>
+        <Table.Row>
+          <Table.ColumnHeader>Product</Table.ColumnHeader>
+          <Table.ColumnHeader>Category</Table.ColumnHeader>
+          <Table.ColumnHeader textAlign="end">Price</Table.ColumnHeader>
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
+        {items.map((item) => (
+          <Table.Row key={item.id}>
+            <Table.Cell>{item.name}</Table.Cell>
+            <Table.Cell>{item.category}</Table.Cell>
+            <Table.Cell textAlign="end">{item.price}</Table.Cell>
+          </Table.Row>
+        ))}
+      </Table.Body>
+    </Table.Root>
+        </Card.Description>
+      </Card.Body>
+      <Card.Footer justifyContent="flex-end">
+        <Button variant="outline">View</Button>
+        <Button>Join</Button>
+      </Card.Footer>
+    </Card.Root>
+    
+  )
+}
+
+const items = [
+  { id: 1, name: "Laptop", category: "Electronics", price: 999.99 },
+  { id: 2, name: "Coffee Maker", category: "Home Appliances", price: 49.99 },
+  { id: 3, name: "Desk Chair", category: "Furniture", price: 150.0 },
+  { id: 4, name: "Smartphone", category: "Electronics", price: 799.99 },
+  { id: 5, name: "Headphones", category: "Accessories", price: 199.99 },
+]
+
+
+export default Demo;
